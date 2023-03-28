63d0f73a854bee6228e53ee9ae4fe9fc60058ac7
fosware <fosahadal@protonmail.com>
2023-03-27 20:01:48 -0600

    first commit, create app

---
+++ src/tasks/task.entity.ts
@@ -0,0 +1,12 @@
+enum TastStatus {
+  PENDING = 'PENDING',
+  IN_PROGRESS = 'IN_PROGRESS',
+  DONE = 'DONE',
+}
+
+export class Task {
+  id: string;
+  title: string;
+  description: string;
+  status: TastStatus;
+}
